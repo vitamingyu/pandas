dc636c2902a14e45bf57f1a5bb53a40713b39bae
vitamingyu <codemin9@gmail.com>
2023-11-22 19:04:41 +0900

    텐서플로 기본 연산

--- pypro5tf/tf_pack1/tf1.py
+++ pypro5tf/tf_pack1/tf1.py
@@ -5,5 +5,36 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
-print(tf.__version__)
-print('GPU사용 가능' if tf.test.is_gpu_available() else 'GPU사용 불가능 ㅠ')
+
+# print(tf.__version__)
+# print('GPU사용 가능' if tf.test.is_gpu_available() else 'GPU사용 불가능 ㅠ')
+# print(tf.config.list_physical_devices('GPU'))
+
+print('tensor : 수치용 컨테이너다. 임의의 차원을 가지는 행렬의 일반화된 모습이다. 계산 그래프 구조를 갖음. 병렬연산이 기본')
+print(1, type(1))  # 1 <class 'int'>
+print(tf.constant(1), type(tf.constant(1)))  # tf.Tensor(1, shape=(), dtype=int32) <class 'tensorflow.python.framework.ops.EagerTensor'> 0-d tensor : scalar
+print(tf.constant([1]), ' ', tf.rank(tf.constant([1])))  # 1-d tensor : vector  1차원
+print(tf.constant([[1]]))  # 2-d tensor : matrix  2차원
+print()
+a = tf.constant([1,2])
+b = tf.constant([3,4])
+c = a + b
+print(c)
+d = tf.add(a,b)
+print(d)
+print()
+print(7)  # 파이썬
+# 아래는 7이라는 값이 다 텐서로 바뀜. 4가지 방법
+print(tf.convert_to_tensor(7, dtype=tf.float32))
+print(tf.cast(7, dtype=tf.float32))
+print(tf.constant(7.0))
+print(tf.constant(7, dtype=tf.float32))
+
+print()
+import numpy as np
+arr = np.array([1,2])
+print(arr, type(arr))
+tfarr = tf.add(arr, 5)
+print(tfarr)
+print(tfarr.numpy())  # 텐서를 다시 넘파이로 바꾸고 싶다면
+print(np.add(tfarr, 3))
